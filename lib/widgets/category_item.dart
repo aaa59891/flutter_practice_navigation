4f0b8b282828f6b9210a11dcfbec6e148cba5d44
Chong <aaa59891@gmail.com>
2019-11-03 18:33:01 +0800

    add category item widget

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+  const CategoryItem(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text(category.title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(.7),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
