1b3ef778ba75a09539b1d01a3806b3bf41a93d27
Chong <aaa59891@gmail.com>
2019-11-04 22:47:08 +0800

    add card theme

---
+++ lib/constants/styles.dart
@@ -0,0 +1,3 @@
+class CardStyle {
+  static const double borderRadius = 15;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/constants/styles.dart';
 import 'package:my_app/routes.dart';
 import 'package:my_app/screens/categories_screen.dart';
 import 'package:my_app/screens/category_meals_screen.dart';
@@ -16,6 +17,13 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.amber,
         fontFamily: 'Raleway',
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        cardTheme: ThemeData.light().cardTheme.copyWith(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(CardStyle.borderRadius),
+              ),
+              margin: EdgeInsets.all(10),
+            ),
         textTheme: ThemeData.light().textTheme.copyWith(
               body1: TextStyle(
                 color: Color.fromRGBO(20, 51, 51, 1),
