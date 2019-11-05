346ff65918469b5cf65a7a30b5626adbeb2e7714
Chong <aaa59891@gmail.com>
2019-11-05 23:08:01 +0800

    finish meal detail screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -2,6 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:my_app/models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
+  Widget buildSectionTitle(String title, BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+  Widget buildContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      height: 150,
+      width: 300,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context).settings.arguments as Meal;
@@ -9,7 +34,52 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('${meal.title}'),
       ),
-      body: Text('meal'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            this.buildSectionTitle('Ingredients', context),
+            this.buildContainer(
+              ListView.builder(
+                itemBuilder: (_, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  margin: EdgeInsets.all(5),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                ),
+                itemCount: meal.ingredients.length,
+              ),
+            ),
+            this.buildSectionTitle('Steps', context),
+            this.buildContainer(ListView.builder(
+              itemBuilder: (_, index) => Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('# ${index + 1}'),
+                    ),
+                    title: Text(meal.steps[index]),
+                  ),
+                  Divider(),
+                ],
+              ),
+              itemCount: meal.steps.length,
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
